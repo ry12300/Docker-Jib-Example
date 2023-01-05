d48954811a8e04655ca35d5c6a7edd1e9f78c7dd
ry12300 <57409558+ry12300@users.noreply.github.com>
2023-01-05 17:19:32 +0000

    Add files via upload

---
+++ src/main/java/com/example/jibDockerExample/JibDockerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.jibDockerExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JibDockerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JibDockerExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jibDockerExample/JibDockerExampleApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.jibDockerExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JibDockerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {}
+
+}
